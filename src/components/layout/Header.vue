fc9866e457ce91b4aa0a9ff67dbe5bc6a9980c6d
Замалдинов Равиль <vanteateam@gmail.com>
2024-10-13 18:47:16 +0500

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,9 +2,11 @@
 import SettingStore from '@/stores/settings.js'
 import TgIcon from '@/assets/icons/tg.svg?raw'
 import { useI18n } from 'vue-i18n'
-const { t, locale } = useI18n()
+import {computed} from "vue";
+const { t, locale } = useI18n({ useScope: 'global' })
 
 const { data: settings } = SettingStore
+const url = computed(() => settings.chat[locale])
 </script>
 
 <template>
@@ -13,7 +15,7 @@ const { data: settings } = SettingStore
 			<div class="between">
 				<logo></logo>
 				<spacer>
-					<a :href="settings.chat[locale]" target="_blank">
+					<a :href="url" target="_blank">
 						<v-button svg :icon="TgIcon" :round="false">
 							{{ t('header.chat') }}
 						</v-button>
